e294c39b6b5b1335846704ff9a75550f9c984bb3
Joshhortt <joshhortt@yahoo.com>
2021-04-26 14:05:49 +0100

    Initialize Dapper Setup in Library. Add connection string to LoadData method.Add IConfiguration.

---
+++ HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace HotelAppLibrary.Databases
+{
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+            public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString("");
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                return rows;
+            }
+        }
+    }
+}
+
