171894c0d3bc318d7cd3886fd50f84d32cb9096b
Joshhortt <joshhortt@yahoo.com>
2021-04-27 20:23:01 +0100

    Implement Check In

--- HotelAppLibrary/Data/SqlData.cs
+++ HotelAppLibrary/Data/SqlData.cs
@@ -67,5 +67,10 @@ namespace HotelAppLibrary.Data
                                                     connectionStringName,
                                                     true); 
         }
+
+        public void CheckInGuest(int bookingId)
+        {
+            _db.SaveData("dbo.spBookings_CheckIn", new { Id = bookingId }, connectionStringName, true);
+        }
     }
 }
