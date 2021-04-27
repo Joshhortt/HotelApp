7443af98d9ec696fd7775e48f18e161560cbdd45
Joshhortt <joshhortt@yahoo.com>
2021-04-27 09:26:38 +0100

    Create properties class 'RoomTypeModel' & class SqlData. Insert Initial Implementation of connection with Db.

---
+++ HotelAppLibrary/Data/SqlData.cs
@@ -0,0 +1,23 @@
+﻿using HotelAppLibrary.Databases;
+using HotelAppLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelAppLibrary.Data
+{
+    public class SqlData
+    {
+        private readonly ISqlDataAccess _db;
+        private const string connectionStringName = "SqlDb";
+
+        public SqlData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+            return _db.LoadData<RoomTypeModel, dynamic>("dbo.spRoomTypes_GetAvailableTypes",  new { startDate, endDate }, "Sqldb", true);
+        }
+    }
+}
